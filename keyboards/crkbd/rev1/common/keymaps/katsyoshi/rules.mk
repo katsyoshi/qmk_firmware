f24a6fa20a3c56b052bca99c735d4231562f352d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2021-02-15 00:48:11 +0900

    add crkbd

---
+++ keyboards/crkbd/rev1/common/keymaps/katsyoshi/rules.mk
@@ -0,0 +1,5 @@
+MOUSEKEY_ENABLE     = no     # Mouse keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+OLED_DRIVER_ENABLE  = no
+LTO_ENABLE          = yes
